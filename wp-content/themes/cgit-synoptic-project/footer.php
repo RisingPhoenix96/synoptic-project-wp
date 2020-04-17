882d9730ce033ba32f4469b774ed0384a499e53c
Lamé Moatshe <lame@castlegateit.co.uk>
2020-04-17 16:44:49 +0100

    footer

--- wp-content/themes/cgit-synoptic-project/footer.php
+++ wp-content/themes/cgit-synoptic-project/footer.php
@@ -1,3 +1,13 @@
+<?php if (!is_page_template('templates/home.php')) : ?>
+    <footer>
+        <div>
+            <div class="page-wrap">
+                <p class="site-footer-meta-company-info">Copyright © <?= date('Y') ?> <?= bloginfo() ?>. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+<?php endif; ?>
+
 <?php wp_footer() ?>
 
 </body>
